e0f9ad619fff4e1e251a0c7739cb3587d8b5291e
GinaMaddox <gina.maddox@gmail.com>
2018-06-18 21:26:40 -0400

    Model Store Item

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+shoes1 = {:brand => "Nike", :color => "blue", :price => 50}
+shoes2 = {:brand => "Adidas", :color =>"black", :price => 30}
+shoes3 = {:brand => "Cloud", :color =>"pink", :price => 40}
+
+p shoes1
