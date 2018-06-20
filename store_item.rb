76f7c645e9f5ab223a57e626bacd6af03840bfe4
GinaMaddox <gina.maddox@gmail.com>
2018-06-19 20:01:47 -0400

    add input options with initialize, #hash shortcuts

--- store_item.rb
+++ store_item.rb
@@ -11,10 +11,10 @@ class Shoes
   attr_reader :brand, :color, :price
   attr_writer :color
 
-  def initialize (input_brand, input_color, input_price)
-    @brand = input_brand
-    @color = input_color
-    @price = input_price
+  def initialize (input_options)
+    @brand = input_options[:brand]
+    @color = input_options[:color]
+    @price = input_options[:price]
   end
 
   def print_item
@@ -26,18 +26,20 @@ class Shoes
   end
 end
 
-shoes1 = Shoes.new("Nike", "blue", 50)
-shoes2 = Shoes.new("Adidas", "black", 60)
-shoes3 = Shoes.new("Cloud", "pink", 70)
+shoes1 = Shoes.new(brand: "Nike", color: "blue", price: 50) #fancy 
+shoes2 = Shoes.new({:brand => "Adidas", :color =>"black", :price => 30}) #explicit example
+shoes3 = Shoes.new(brand: "Cloud", color: "pink", price: 40)
 
-shoes1.print_item
-shoes1.add_discount
-shoes1.print_item
+#shoes1.print_item
+#shoes1.add_discount
+#shoes1.print_item
 
 
 #p shoes2.color
-shoes2.color = "white"
+#shoes2.color = "white"
 #p shoes2.color
 #shoes2.print_item
 #shoes3.print_item
 
+p shoes1.brand
+p shoes2.color
